516f5d091f0bf2b6b98aa04bd77b661c647c3f33
Bibhot7 <bibhot7@outlook.com>
2024-01-28 07:30:34 +1300

    Add project files.

---
+++ more on tryparse/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace more_on_tryparse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString = "128";
+            int parsedValue;
+            bool success = int.TryParse(numberAsString, out parsedValue);
+            if (success) { Console.WriteLine("parsing successful " +parsedValue); }
+            else { Console.WriteLine("parsing failed"); }
+        }
+    }
+}
